a92b89255adc55fd12888eb3687c353c6cc1454b
weiT1993 <tangwei1027@gmail.com>
2021-05-11 12:38:58 -0400

    Add README, test

---
+++ test.py
@@ -0,0 +1,26 @@
+from qiskit_helper_functions.non_ibmq_functions import generate_circ
+
+from cutqc.main import CutQC
+
+if __name__ == '__main__':
+    # Generate some test circuits
+    circuits = {}
+    max_subcircuit_qubit = 10
+    for full_circ_size in [15]:
+        circuit = generate_circ(full_circ_size=full_circ_size, circuit_type='supremacy')
+        circuit_name = 'supremacy_%d'%full_circ_size
+        if circuit.num_qubits==0:
+            continue
+        else:
+            circuits[circuit_name] = {
+                'circuit':circuit,'max_subcircuit_qubit':max_subcircuit_qubit,'max_cuts':30,'num_subcircuits':[2,3,4,5]}
+
+    # Use CutQC package to evaluate the circuits
+    num_nodes = 1
+    num_threads = 1
+    qubit_limit = 24
+    eval_mode = 'sv'
+    cutqc = CutQC(verbose=True)
+    cutqc.cut(circuits=circuits)
+    cutqc.evaluate(circuits=circuits,eval_mode=eval_mode,qubit_limit=qubit_limit,num_nodes=num_nodes,num_threads=num_threads,ibmq=None)
+    errors = cutqc.verify(circuits=circuits,num_nodes=num_nodes,num_threads=num_threads,qubit_limit=qubit_limit,eval_mode=eval_mode)
