b1b63c80f8d7843d9eba01d16ffec4be2145f4db
weiT1993 <tangwei1027@gmail.com>
2021-07-10 14:00:20 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='CutQC',
+    version='0.0.1',
+    description='CutQC package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/weiT1993/CutQC.git',
+    packages=setuptools.find_packages(),
+    author='weiT1993',
+    author_email='tangwei1027@gmail.com',
+    license='MIT',
+    python_requires='>=3.7',
+    zip_safe=False)
