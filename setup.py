d5cd19d91e6d5b3953f631539979dfc5d2d23629
weiT1993 <tangwei1027@gmail.com>
2022-04-05 14:53:59 -0400

    modified gitignore

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='old_cutqc',
+    name='cutqc',
     version='0.0.1',
     description='CutQC Backend',
     long_description=long_description,
